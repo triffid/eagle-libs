4857b2def36528fd9bd5ab43509a13d5823db2cf
Michael Moon <triffid.hunter@gmail.com>
2021-03-09 17:53:42 +0800

    LM5069: improve library for better automatic BOM, rotate footprint, add a few more values to LM5069.py ancillary calculator

--- LM5069.py
+++ LM5069.py
@@ -2,19 +2,22 @@
 #-*- coding: utf8 -*-
 
 # low voltage turn-on threshold
-Vuvh = 36
+Vuvh = 32
 
 # low voltage turn-off threshold
-Vuvl = 32
+Vuvl = 29.5
 
 # high voltage turn-off threshold
-Vovh = 54
+Vovh = 62
 
 # MOSFET Power limit, watts
-Plim = 2
+Plim = 30
 
 # current limit, amps
-Imax = 0.33
+Imax = 10
+
+# capacitance downstream of the controller
+Cout = 2.2e-6 * 11
 
 import sys
 from pprint import pprint, pformat
@@ -36,12 +39,12 @@ eseries = {
 	192: [100, 101, 102, 104, 105, 106, 107, 109, 110, 111, 113, 114, 115, 117, 118, 120, 121, 123, 124, 126, 127, 129, 130, 132, 133, 135, 137, 138, 140, 142, 143, 145, 147, 149, 150, 152, 154, 156, 158, 160, 162, 164, 165, 167, 169, 172, 174, 176, 178, 180, 182,184, 187, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213, 215, 218, 221, 223, 226, 229, 232, 234, 237, 240, 243, 246, 249, 252, 255, 258, 261, 264, 267, 271, 274, 277, 280, 284, 287, 291, 294, 298, 301, 305, 309, 312, 316, 320, 324, 328, 332, 336, 340, 344, 348, 352, 357, 361, 365, 370, 374, 379, 383, 388, 392, 397, 402, 407, 412, 417, 422, 427, 432, 437, 442, 448, 453, 459, 464, 470, 475, 481, 487, 493, 499, 505, 511, 517, 523, 530, 536, 542, 549, 556, 562, 569, 576, 583, 590, 597, 604, 612, 619, 626, 634, 642, 649, 657, 665, 673, 681, 690, 698, 706, 715, 723, 732, 741, 750, 759, 768, 777, 787, 796, 806, 816, 825, 835, 845, 856, 866, 876, 887, 898, 909, 920, 931, 942, 953, 965, 976, 988]
 }
 
-def closest(val):
+def closest(val, eser = eseries[24]):
 	mul1 = 10 ** (ceil(log10(val)) - 2)
 	mul2 = 10 ** (floor(log10(val)) - 2)
 	best = 0
 	besterror = inf
-	for r in eseries[24]:
+	for r in eser:
 		if (fabs((r * mul1) - val) < fabs(besterror)):
 			best = r * mul1
 			besterror = (r * mul1) - val
@@ -50,14 +53,21 @@ def closest(val):
 			besterror = (r * mul2) - val
 	return [best, besterror]
 
-# low voltage turn-on threshold
-Vuvh = 36
-
-# low voltage turn-off threshold
-Vuvl = 32
-
-# high voltage turn-off threshold
-Vovh = 54
+def higher(val, eser = eseries[24]):
+	mul1 = 10 ** (ceil(log10(val)) - 2)
+	mul2 = 10 ** (floor(log10(val)) - 2)
+	best = 0
+	besterror = inf
+	for r in eser:
+		if  (((r * mul1) - val) > 0) and \
+			(((r * mul1) - val) < (besterror)):
+			best = r * mul1
+			besterror = (r * mul1) - val
+		if  (((r * mul2) - val) > 0) and \
+			(((r * mul2) - val) < (besterror)):
+			best = r * mul2
+			besterror = (r * mul2) - val
+	return [best, besterror]
 
 R1 = (Vuvh - Vuvl) / 0.000021
 
@@ -95,11 +105,16 @@ print("Vovl = %sv (falling turn-on)" % siprefix(Vovl))
 
 Rsns = 0.0485 / Imax
 
-res = closest(Rsns)
+#Rsns = 0.005
+
+res = closest(Rsns, [200, 250, 300, 400, 500, 600, 800, 900] + eseries[12])
+
+#res = [ 0.005, 0 ]
 
 print("Rsns = %sΩ -> %sΩ (%+1.2f%%), %sW -> %sA" % (siprefix(Rsns), siprefix(res[0]), res[1] / res[0] * 100, siprefix(Imax * Imax * res[0]), siprefix(0.0485 / Rsns)))
 
 Rsns = res[0]
+Imax = 0.0485 / Rsns
 
 Plimmin = 0.005 * Vovh / Rsns
 
@@ -108,6 +123,21 @@ if (Plim < Plimmin):
 
 Rpwr = 1.3e5 * Rsns * (Plim - 0.00118 * Vovh / Rsns)
 
-res = closest(Rpwr)
+try:
+	res = closest(Rpwr)
+	Plim = res[0] / 1.3e5 / Rsns + 0.00118 * Vovh / Rsns
+	print("Rpwr = %sΩ -> %sΩ (%+1.2f%%) -> %sW" % (siprefix(Rpwr), siprefix(res[0]), res[1] / res[0] * 100, siprefix(Plim)))
+except:
+	print("Rpwr value %gΩ is nonsense, lowest power is %sW @ Rpwr = 0" % (Rpwr, siprefix(0.00118 * Vovh / Rsns)))
+	Plim = 0.00118 * Vovh / Rsns
 
-print("Rpwr = %sΩ -> %sΩ (%+1.2f%%) -> %sW" % (siprefix(Rpwr), siprefix(res[0]), res[1] / res[0] * 100, siprefix(res[0] / 1.3e5 / Rsns + 0.00118 * Vovh / Rsns)))
+
+tStart = Cout / 2 * ((Vovh ** 2) / Plim + Plim / (Imax ** 2))
+
+Ctimer = tStart * 85e-6 / 4 * 1.5
+
+res = higher(Ctimer, eseries[3])
+
+print("tStart = %ss, Ctimer = %sF -> %sF (%+1.2f%%)" % (siprefix(tStart), siprefix(Ctimer), siprefix(res[0]), res[1] / res[0] * 100))
+
+print("tFault = %ss" % (siprefix(res[0] * 4 / 85e-6)))
